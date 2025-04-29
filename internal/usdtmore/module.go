1e871dd45f2a2e8a70d37701775d75de50fd4c3f
lonestech <cjszxzx520@163.com>
2025-04-29 23:55:29 +0800

    update

--- internal/usdtmore/module.go
+++ internal/usdtmore/module.go
@@ -2,26 +2,28 @@ package usdtmore
 
 import (
 	"github.com/imroc/req/v3"
+	"github.com/topjohncian/cloudreve-pro-epay/internal/appconf"
 	"go.uber.org/fx"
 )
 
 // Module USDTMore 模块
 var Module = fx.Options(
 	fx.Provide(
-		NewConfigFromEnv,
+		fx.Annotate(
+			NewConfigFromEnv,
+			fx.ParamTags(`group:"config"`),
+		),
 		ProvideClient,
 	),
 )
 
 // NewConfigFromEnv 从环境变量创建配置
-func NewConfigFromEnv() *Config {
-	// 这里实际应该从环境变量读取配置
-	// 暂时使用硬编码的配置，后续会修改为从环境变量读取
+func NewConfigFromEnv(conf *appconf.Config) *Config {
 	return &Config{
-		Enabled:      true,
-		APIEndpoint:  "http://localhost:6080",
-		AuthToken:    "123456",
-		DefaultChain: "TRON",
+		Enabled:      conf.USDTMoreEnabled,
+		APIEndpoint:  conf.USDTMoreAPIEndpoint,
+		AuthToken:    conf.USDTMoreAuthToken,
+		DefaultChain: conf.USDTMoreDefaultChain,
 	}
 }
 
